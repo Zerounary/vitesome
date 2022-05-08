5268347db21518b01d8557877203fbb0ad8008c2
Zerounary <850657663@qq.com>
2022-05-08 17:59:16 +0800

    使用 @ 代表根目录

--- vite.config.ts
+++ vite.config.ts
@@ -16,8 +16,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '/@': resolve(__dirname, './src'),
-      '/~': resolve(__dirname, './'),
+      '@': resolve(__dirname, './src'),
+      '~': resolve(__dirname, './'),
     },
   },
   plugins: [
