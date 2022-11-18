39f8e0bbbd790c22d46a1602c51c221a7043c672
Zerounary <song.c@sungeon.com>
2022-11-18 10:23:49 +0800

    fix: 修复使用 storage 的 测试冲突

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 export const assetsUrl = (path = '') => {
   return `${import.meta.env.VITE_ASSETS_URL}/${path}`
 }
+
+export const storage = typeof(global) === 'undefined' ? localStorage : {} as Storage;
